114498703448a1c82569c8d58011bb2917d4fb86
Lucas Rodrigues <ludecaro@outlook.com>
2023-12-19 23:52:01 -0300

    Subindo arquivos de Infra

---
+++ Infra/beanstalk.tf
@@ -0,0 +1,45 @@
+#Documentação: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elastic_beanstalk_application
+resource "aws_elastic_beanstalk_application" "aplicacao_beanstalk" {
+  name        = var.nome
+  description = var.descricao
+}
+
+#Documentação de environments: https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html
+resource "aws_elastic_beanstalk_environment" "ambiente_beanstalk" {
+  name                = var.ambiente
+  application         = aws_elastic_beanstalk_application.aplicacao_beanstalk.name
+  solution_stack_name = "64bit Amazon Linux 2 v3.6.5 running Docker"
+
+setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "InstanceType"
+    value = var.maquina
+}
+
+setting {
+    namespace = "aws:autoscaling:asg"
+    name = "MaxSize"
+    value = var.max
+}
+
+setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name = "IamInstanceProfile"
+    value = aws_iam_instance_profile.beanstalk_ec2_profile.name
+
+}
+
+}
+
+resource "aws_elastic_beanstalk_application_version" "default" {
+  depends_on = [
+    aws_elastic_beanstalk_environment.ambiente_beanstalk,
+    aws_elastic_beanstalk_application.aplicacao_beanstalk,
+    aws_s3_bucket_object.docker
+  ]
+  name        = var.ambiente
+  application = var.nome
+  description = var.descricao
+  bucket      = aws_s3_bucket.beanstalk_deploys.id
+  key         = aws_s3_bucket_object.docker.id
+}
--- Infra/variaveis.tf
+++ Infra/variaveis.tf
@@ -1,3 +1,15 @@
 variable "nome" {
     type = string
 }
+variable "descricao" {
+    type = string
+}
+variable "max" {
+    type = number
+}
+variable "maquina" {
+    type = string
+}
+variable "ambiente" {
+    type = string
+}
