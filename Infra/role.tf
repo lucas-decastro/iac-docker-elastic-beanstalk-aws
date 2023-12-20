72db1b5ad65dbccf86c830aba1c1364c4b747d41
Lucas Rodrigues <ludecaro@outlook.com>
2023-12-19 22:00:39 -0300

    subindo arquivo role.tf

---
+++ Infra/role.tf
@@ -0,0 +1,60 @@
+#Documentação Terraform: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
+resource "aws_iam_role" "beanstalk_ec2" {
+  name = "beanstalk-ec2-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "beanstalk_ec2_policy" {
+  name = "beanstalk-ec2-policy"
+  role = aws_iam_role.beanstalk_ec2.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "cloudwatch:PutMetricData",
+          "ds:CreateComputer",
+          "ds:DescribeDirectories",
+          "ec2:DescribeInstancesStatus",
+          "logs:*",
+          "ssm:*",
+          "ec2messages:*",
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
+          "ecr:DescribeImages",
+          "ecr:BatchGetImage",
+          "s3:*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "beanstalk_ec2_profile" {
+    name = "beanstalk-ec2-profile"
+    role = aws_iam_role.beanstalk_ec2.name
+}
